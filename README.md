2edd77e6068481d71188a4f1a848b7c458ac1ee8
josecalvosuarez <jose.calvo.suarez@una.ac.cr>
2023-11-10 20:34:00 -0600

    feat: inicializando repositorio

---
+++ README.md
@@ -0,0 +1,1 @@
+Repositorio de pruebas de estructuras de datos
---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+def generar_random(x, y):
+    return random.randint(x, y)
+
+print("Numero aleatorio: {}".format(generar_random(1, 100)))
