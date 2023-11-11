9530b87c14b27487cf7a0ff09000685c1265f090
josecalvosuarez <jose.calvo.suarez@una.ac.cr>
2023-11-10 20:55:08 -0600

    feat: agrega diccionario

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def generar_random(x, y):
     return random.randint(x, y)
 
 print("Numero aleatorio: {}".format(generar_random(1, 100)))
+
+d = dict()
+
+d["cervecita"] = "Imperial"
+d["campeon"] = "LDA"
+
+print(d)
